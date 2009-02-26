749dc0aab045637fbcba7ad37f1ddc7d443674c4
guilhermewachs <guilhermewachs@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-02-26 19:18:37 +0000

    Atualização de arquivos conforme pedido do Andrey. Estes arquivos estão relacionados com o login no sistema.

--- src/br/edu/fei/sigepapp/servlet/LoginServlet.java
+++ src/br/edu/fei/sigepapp/servlet/LoginServlet.java
@@ -1,7 +1,7 @@
 package br.edu.fei.sigepapp.servlet;
 
 /**
- * @(#)LoginServlet.java 0.01 09/02/16
+ * @(#)LoginServlet.java 0.03 09/02/25
  * 
  * Copyright (c) 2009 Equipe SiGePAPP
  * 
@@ -19,40 +19,55 @@ package br.edu.fei.sigepapp.servlet;
  * |------------------------------------------------------------------|
  * |   Andrey    |   16/02/09  |   Criação do programa.               |
  * |------------------------------------------------------------------|
+ * |   Andrey    |   23/02/09  |   Implementação da função para login |
+ * |             |             | utilizando ajax.                     |
+ * |------------------------------------------------------------------|
+ * |   Andrey    |  25/02/09   |   Adequação da classe para efetuar o |
+ * |             |             | Login e o Logout                     |
+ * |------------------------------------------------------------------|
  * 
  */
 
 /** Import dos packages necessários para o funcionamento da classe */
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+//import javax.servlet.http.HttpSession;
+
+import br.edu.fei.sigepapp.bancodedados.dao.UsuarioDAO;
+import br.edu.fei.sigepapp.bancodedados.model.Usuario;
+import br.edu.fei.sigepapp.log.GravarLog;
 
 /**
- * Classe reponsavél pela validação do login dos usuários do sistema
+ * Classe reponsavel pela validação do login dos usuarios do sistema
  * 
- * @version 0.01 16 Fev 2009
+ * @version 0.03 25 Fev 2009
  * @author Andrey Masiero
  * 
  */
 
 public class LoginServlet extends HttpServlet {
+	
 	private static final long serialVersionUID = 1L;
-	/** Atributo url que indica o caminho da pagina que será direcionada a pagina após validação. 
-	 *  Por default passarei o erro como 1, dizendo que ocorreu um erro na validacao do login. 
+	/**  
+	 *  Atributo que passa o nome do usuario para a pagina. 
 	 */
-	private String url;
+	private String user;
 	
 	/** Metodos getters and setters */
-	public String getUrl() {
-		return url;
-		
+	public void setUser(String user) {
+		this.user = user;
 	}
 
-	public void setUrl(String url) {
-		this.url = url;
+	public String getUser() {
+		return user;
 	}
 
 	/**
@@ -71,7 +86,7 @@ public class LoginServlet extends HttpServlet {
 	 * @see HttpServelt#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
-		processRequest(request, response);
+		
 	}
 
 	/**
@@ -80,37 +95,66 @@ public class LoginServlet extends HttpServlet {
 	 * @see HttpServelt#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
-		processRequest(request, response);
+		
+		/** Atributos auxiliares */
+		String cfm = new String();
+		String strNome = new String();
+
+		try {
+			// Retorna o usuario encontrado no cadastro do banco. Retorna null se nao for encontrado.
+			strNome = retornaUsuario(request.getParameter("usuario"), request.getParameter("senha"));
+		} catch (SQLException e) {
+			// Grava log de erro no sistema.
+			GravarLog.gravaErro(LoginServlet.class.getName() + "erro de SQL ao efetuar login no sistema: " + e.getMessage());
+		}
+		// Verifica se o nome e null para acionar o flag de confirmacao para a funcao ajax
+		if (strNome.equals("")){
+			cfm = "0"; // Flag desligado
+		}else{
+			cfm = "1"; // Flag ligado
+		}
+		
+		// Verifica se existe a solicitacao de logoff
+		if (request.getParameter("logoff").equals("")){
+			HttpSession sessao = request.getSession(); // Nao cria a sessao
+			sessao.setAttribute("usuario", strNome); // Seta o nome do usuario na sessao
+		}else if (request.getParameter("logoff").equals("logoff") || cfm.equals("0")){ // Se houver solicitacao
+			HttpSession sessao = request.getSession(); // Pega a sessao
+			sessao.invalidate(); // Mata a sessao
+		}
+		
+		response.setContentType("text/xml"); // informa o tipo de resposta que será gerada pela servlet, no caso arquivo xml
+		PrintWriter out = response.getWriter(); // Inicia a gravacao do arquivo xml
+		out.println("<login><usuario>" + strNome + "</usuario><confirma>"+ cfm +"</confirma></login>"); //conteudo do arquivo
+		out.flush(); // atualiza o conteudo do arquivo
+		out.close(); // fecha o arquivo
 	}
 	
 	/**
-	 * Metodo responsavel pela validacao do login e senha do usuario
+	 * Metodo de busca de usuario e verificacao do login no sistema
 	 * 
-	 * @param request
-	 * @param response
-	 * @throws ServletException
-	 * @throws IOException
-	 * 
+	 * @param login
+	 * @param senha
+	 * @return O usuario encontrado do sistema ou nulo caso nao encontre o usuario
+	 * @throws SQLException
 	 */
-	private void processRequest(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException{
+	private String retornaUsuario(String login, String senha) throws SQLException{
 		
-		setUrl("index.jsp?e=1");
+		// Cria um objeto dao da classe usuario
+		UsuarioDAO dao = new UsuarioDAO();
 		
-		if(request.getParameter("acao").equals("login")){
-			if(request.getParameter("usuario").equals("admin")){
-				if(request.getParameter("senha").equals("admin")){
-					HttpSession sessao = request.getSession();
-					sessao.setAttribute("login", request.getParameter("login"));
-					setUrl("index.jsp?e=0");
-				}
-			}
-		}else if(request.getParameter("acao").equals("logout")){
-			HttpSession sessao = request.getSession();
-			sessao.invalidate();
-			setUrl("index.jsp");
+		// Cria um lista de registros encontrados, neste caso nao pode ser maior do que 1 por se tratar do login
+		List<Usuario> user_list = dao.seleciona("select * from appp_tb_user u where cd_user = " +
+				"(select cd_user from appp_tb_login l where l.nm_login = '" + login + "' and l.pw_senha = '" + senha + "')");
+		
+		// Verifica se o tamanho da lista é realmente 1
+		if(user_list.size() == 1){
+			this.setUser(user_list.get(0).getNm_prim_nome() + " " + user_list.get(0).getNm_ult_nome()); // Caso positivo retorna os Nome e Ultimo Nome do usuario
+		}else{
+			this.setUser(""); // Caso contrario retorna vazio
 		}
 		
-		response.sendRedirect(getUrl());
+		return this.getUser(); // Retorna o valor de usuario
 	}
 }
-
+	
