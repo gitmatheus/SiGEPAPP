df102d735a2797dd39e47018f142188f5165884c
guilhermewachs <guilhermewachs@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-03-02 00:44:45 +0000

    Uso de switch para preenchimento dos campos do objeto Atributo

--- src/br/edu/fei/sigepapp/bancodedados/dao/AtributoDAO.java
+++ src/br/edu/fei/sigepapp/bancodedados/dao/AtributoDAO.java
@@ -14,6 +14,8 @@ import java.util.List;
 import br.edu.fei.sigepapp.bancodedados.ConnectionFactory;
 import br.edu.fei.sigepapp.bancodedados.model.Atributo;
 import br.edu.fei.sigepapp.log.GravarLog;
+import com.sun.net.ssl.internal.ssl.Debug;
+import java.util.Vector;
 
 /**
  *
@@ -27,6 +29,56 @@ public class AtributoDAO {
         this.conn = ConnectionFactory.getConnection();
     }
 
+    public int returnIndex(String selecionado, List<String> CamposDaTabela) {
+        int posEncontrado = -1;
+        for (int i = 0; i < CamposDaTabela.size(); i++) {
+            if (CamposDaTabela.get(i).toUpperCase().equals(selecionado.toUpperCase())) {
+                posEncontrado = i;
+            } else {
+                posEncontrado = -1;
+            }
+        }
+        return posEncontrado;
+    }
+
+    public List<Atributo> PreencheList(ResultSet rs) throws SQLException {
+        // Cria um array do tipo atributo
+        List<Atributo> atributos = new ArrayList<Atributo>();
+        Vector<String> camposDaTabela = new Vector<String>();
+        camposDaTabela.add("CD_ATRIBUTO_OBJ");
+        camposDaTabela.add("NM_ATRIBUTO_OBJ");
+        camposDaTabela.add("DS_ATRIBUTO_OBJ");
+        camposDaTabela.add("DS_TAM_ATRIB");
+        camposDaTabela.add("CD_TIPO");
+        camposDaTabela.add("FL_ATRIB_RELAC");
+        while (rs.next()) {
+            Debug.println("Passei!", "ok");
+            // Cria um objeto do tipo Atributo
+            Atributo atributoNovo = new Atributo();
+
+            for (int i = 1; i <= rs.getMetaData().getColumnCount(); i++) {
+                String nomeColuna = rs.getMetaData().getColumnName(i);
+                switch (returnIndex(nomeColuna, camposDaTabela)) {
+                    case 0:
+                        atributoNovo.setCd_atributo_obj(rs.getLong(i));
+                    case 1:
+                        atributoNovo.setNm_atributo_obj(rs.getString(i));
+                    case 2:
+                        atributoNovo.setDs_atributo_obj(rs.getString(i));
+                    case 3:
+                        atributoNovo.setDs_tam_atrib(rs.getString(i));
+                    case 4:
+                        atributoNovo.setCd_tipo(rs.getLong(i));
+                    case 5:
+                        atributoNovo.setFl_atrib_relac(rs.getString(i));
+                }
+
+            }
+            atributos.add(atributoNovo);
+        }
+        return atributos;
+    }
+
     public List<Atributo> seleciona(String query) {
         try {
             this.conn = ConnectionFactory.getConnection();
@@ -35,18 +87,9 @@ public class AtributoDAO {
 
             // Executa a query e armazenando em um Objeto ResultSet
             ResultSet rs = stmt.executeQuery();
-
-            // Cria um array do tipo atributo
-            List<Atributo> atributos = new ArrayList<Atributo>();
-
             // Enquando a pesquisa nao chegar ao fim ele armazena no array os resultados e permanece no loop
-            while (rs.next()) {
-
-                // Cria um objeto do tipo Atributo
-                Atributo atributoNovo = new Atributo(rs.getLong("CD_ATRIBUTO_OBJ"), rs.getString("NM_ATRIBUTO_OBJ"), rs.getString("DS_ATRIBUTO_OBJ"), rs.getString("DS_TAM_ATRIB"), rs.getLong("CD_TIPO"), rs.getString("FL_ATRIB_RELAC"));
-
-                atributos.add(atributoNovo);
-            }
+            // Cria um array do tipo atributo e referencia com o a lista do metodo preenchelist
+            List<Atributo> atributos = atributos = PreencheList(rs);
 
             // fecha a instancia dos objetos
             rs.close();
@@ -68,7 +111,6 @@ public class AtributoDAO {
             // Retorno da funcao como null em caso de erro
             return null;
         }
-
     }
 
     public boolean adiciona(Atributo atributoAdicionar) {
@@ -95,9 +137,14 @@ public class AtributoDAO {
             GravarLog.gravaInformacao(Atributo.class.getName() + ": insercao no banco de dados realizada com sucesso");
 
             // Fecha conexao com o banco de dados
+
+
             this.conn.close();
 
             // Retorno da funcao como true
+
+
+
             return true;
         } catch (SQLException e) {
 
@@ -105,6 +152,9 @@ public class AtributoDAO {
             GravarLog.gravaErro(Atributo.class.getName() + ": erro na insercao referente a uma excecao de SQL: " + e.getMessage());
 
             // Retorno da funcao como false em caso de erro
+
+
+
             return false;
         }
 
@@ -127,6 +177,9 @@ public class AtributoDAO {
             GravarLog.gravaErro(Atributo.class.getName() + ": erro na insercao referente a uma excecao de SQL: " + e.getMessage());
 
             // Retorno da funcao como false em caso de erro
+
+
+
             return false;
         }
     }
@@ -156,9 +209,14 @@ public class AtributoDAO {
             GravarLog.gravaInformacao(Atributo.class.getName() + ": atualizacao no banco de dados realizada com sucesso");
 
             // Fecha conexao com o banco de dados
+
+
             this.conn.close();
 
             // retorno da funcao como true
+
+
+
             return true;
         } catch (SQLException e) {
 
@@ -166,6 +224,9 @@ public class AtributoDAO {
             GravarLog.gravaErro(Atributo.class.getName() + ": erro na atualizacao referente a uma excecao de SQL: " + e.getMessage());
 
             // Retorno da funcao como false em caso de erro
+
+
+
             return false;
         }
     }
