7d9dcc767a4b9b6cfaa4e66560d948f28c9577ca
toadgeek <toadgeek@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-03-07 16:33:37 +0000

    Scripts Iniciais enviados em 26 de Fevereiro de 2009.

--- Arquivos/ScriptsBanco/APPP_TB_ATRIB_ESTRUTURA.sql
+++ Arquivos/ScriptsBanco/APPP_TB_ATRIB_ESTRUTURA.sql
@@ -4,7 +4,7 @@
 * Author                      : WeeDo
 * History                     : 16/02/2009 - Guilherme Wachs   - Versão Inicial
 *                             : 06/03/2009 - Matheus Gonçalves - Adição de Table Space
-*                                                           - Adição de Primary Key            
+*                                                              - Adição de Primary Key
 ***********************************************************************************************************************/
 -- Create table 
 SELECT 'Criando a tabela APPP_TB_ATRIB_ESTRUTURA' FROM DUAL;
@@ -21,7 +21,7 @@ create table APPP_TB_ATRIB_ESTRUTURA(CD_ESTRUTURA    NUMBER(10,0) NOT NULL,
   );
 -- Create/Recreate PK_APPP_TB_ANTI_PATTERN
 alter table APPP_TB_ATRIB_ESTRUTURA
-  add CONSTRAINT PK_APPP_TB_ATRIB_ESTRUTURA PRIMARY KEY (CD_ESTRUTURA); 
+  add CONSTRAINT PK_APPP_TB_ATRIB_ESTRUTURA PRIMARY KEY (CD_ESTRUTURA,CD_ATRIBUTO_OBJ);
 
 -- Create/Recreate FK_APPP_TB_ANTI_PATTERN
 
