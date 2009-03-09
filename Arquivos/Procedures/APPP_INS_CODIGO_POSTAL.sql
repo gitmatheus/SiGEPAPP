f19124666cdd36a805f7349dd601133cc1befb26
andreyam <andreyam@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-03-09 00:57:51 +0000

    Update do cadastro de usuario,procedure APPP_INS_CODIGO_POSTAL acrescentado atributo cd_estado faltante.

--- Arquivos/Procedures/APPP_INS_CODIGO_POSTAL.sql
+++ Arquivos/Procedures/APPP_INS_CODIGO_POSTAL.sql
@@ -1,30 +1,27 @@
-/**********************************************************************************************************************
-* Project Name           : SiGEPAPP
-* APPP_INS_CODIGO_POSTAL : Procedure para criação de dados de CODIGO_POSTAL
-*                         vResult( 1=OK; -99=ErroGeral)
-* Author                 : WeeDo 
-* History                : 03/03/2009 - Matheus Gonçalves
-***********************************************************************************************************************/
 create or replace procedure APPP_INS_CODIGO_POSTAL(pCD_CEP    IN NUMBER   ,
                                                    pNM_RUA    IN VARCHAR2 ,
                                                    pCD_CIDADE IN NUMBER   ,
+                                                   pCD_ESTADO IN NUMBER   ,
                                                    vResult     out number) is
 begin
-   
+
    insert into APPP_TB_CODIGO_POSTAL(CD_CEP    ,
                                      NM_RUA    ,
-                                     CD_CIDADE 
+                                     CD_CIDADE ,
+                                     CD_ESTADO
                                     )
                              values( pCD_CEP    ,
                                      pNM_RUA    ,
-                                     pCD_CIDADE
+                                     pCD_CIDADE ,
+                                     pCD_ESTADO
                                    );
    vResult := 1; -- OK
    commit;
-   
+
    EXCEPTION
      WHEN OTHERS THEN
         rollback;
         vResult := -99; -- Erro genérico.
-               
+
 end APPP_INS_CODIGO_POSTAL;
+/
