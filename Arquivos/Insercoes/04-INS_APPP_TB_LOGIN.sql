657252f357457ac69ccba8affb0af90ca9d2ac4d
toadgeek <toadgeek@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-03-13 17:41:49 +0000

    Adição de um login.

--- Arquivos/Insercoes/04-INS_APPP_TB_LOGIN.sql
+++ Arquivos/Insercoes/04-INS_APPP_TB_LOGIN.sql
@@ -1,2 +1,5 @@
 REM INSERTING into APPP_TB_LOGIN
 Insert into APPP_TB_LOGIN (CD_USER,NM_LOGIN,PW_SENHA) values (35140189879,'admin','admin');
+Insert into APPP_TB_LOGIN (CD_USER,NM_LOGIN,PW_SENHA) values (30150215835,'toadpunk','toadpunk');
+
+
