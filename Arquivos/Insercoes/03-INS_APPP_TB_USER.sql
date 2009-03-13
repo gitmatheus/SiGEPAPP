2041ef55b7fce0ec72a7aaa764e08c216940ef57
toadgeek <toadgeek@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-03-13 17:40:03 +0000

    Correção de datas. Adição de um user.

--- Arquivos/Insercoes/03-INS_APPP_TB_USER.sql
+++ Arquivos/Insercoes/03-INS_APPP_TB_USER.sql
@@ -1,2 +1,3 @@
 REM INSERTING into APPP_TB_USER
-Insert into APPP_TB_USER (CD_USER,DS_AREA_INTERESSE,DT_CADASTRO,DT_NASC,NM_MSN,NM_PRIM_NOME,NM_SKYPE,NM_ULT_NOME,NR_NOTA) values (35140189879,'Programação;Patterns',to_timestamp('06/03/09','DD/MM/RR HH24:MI:SSXFF'),to_timestamp('23/03/87','DD/MM/RR HH24:MI:SSXFF'),'cmtewachs@hotmail.com','Guilherme','guilherme.wachs','Lopes',5);
+Insert into APPP_TB_USER (CD_USER,DS_AREA_INTERESSE,DT_CADASTRO,DT_NASC,NM_MSN,NM_PRIM_NOME,NM_SKYPE,NM_ULT_NOME,NR_NOTA) values (35140189879,'Programação;Patterns',sysdate,to_date('23/03/1987','DD/MM/YYYY'),'cmtewachs@hotmail.com','Guilherme','guilherme.wachs','Lopes',5);
+Insert into APPP_TB_USER (CD_USER,DS_AREA_INTERESSE,DT_CADASTRO,DT_NASC,NM_MSN,NM_PRIM_NOME,NM_SKYPE,NM_ULT_NOME,NR_NOTA) values (30150215835,'TI;ARGs;Esportes',sysdate,to_date('11/02/1982','DD/MM/YYYY'),'toadbatera@hotmail.com','Matheus','toadpunk','Goncalves',null);
