a53ded6bc2ee89502bf023754abc33ac732039b6
guilhermewachs <guilhermewachs@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-04-10 20:31:11 +0000

    Mais bugs encontrados.

--- Arquivos/Insercoes/06-INS_APPP_TB_ATRIBUTO_OBJ.sql
+++ Arquivos/Insercoes/06-INS_APPP_TB_ATRIBUTO_OBJ.sql
@@ -9,25 +9,14 @@ Insert into APPP_TB_ATRIBUTO_OBJ (CD_TIPO,DS_ATRIBUTO_OBJ,FL_ATRIB_RELAC,NM_ATRI
       values (1,'Nome do documento','N','Nome',APPP_FN_NM_COL_ATRIB('Nome'));
 
 Insert into APPP_TB_ATRIBUTO_OBJ (CD_TIPO,DS_ATRIBUTO_OBJ,FL_ATRIB_RELAC,NM_ATRIBUTO_OBJ,NM_COLUNA) 
-<<<<<<< .mine
       values (1,'Onde o problema é encontrado','N','Contexto',APPP_FN_NM_COL_ATRIB('Contexto') );
-=======
-      values (5,'Onde o problema é encontrado','N','Contexto',null);
->>>>>>> .r267
 
 Insert into APPP_TB_ATRIBUTO_OBJ (CD_TIPO,DS_ATRIBUTO_OBJ,FL_ATRIB_RELAC,NM_ATRIBUTO_OBJ,NM_COLUNA) 
-<<<<<<< .mine
       values (1,'Descrição do problema','N','Problema',APPP_FN_NM_COL_ATRIB( 'Problema'));
-=======
-      values (5,'Descrição do problema','N','Problema',null);
->>>>>>> .r267
 
 Insert into APPP_TB_ATRIBUTO_OBJ (CD_TIPO,DS_ATRIBUTO_OBJ,FL_ATRIB_RELAC,NM_ATRIBUTO_OBJ,NM_COLUNA) 
-<<<<<<< .mine
       values (1,'Descrição da solução','S','Solução',APPP_FN_NM_COL_ATRIB('Solução'));
-=======
-      values (5,'Descrição da solução','S','Solução',null);
->>>>>>> .r267
+
 
 Insert into APPP_TB_ATRIBUTO_OBJ (CD_TIPO,DS_ATRIBUTO_OBJ,FL_ATRIB_RELAC,NM_ATRIBUTO_OBJ,NM_COLUNA) 
       values (1,'Descrição de Barreiras','S','Barreiras',APPP_FN_NM_COL_ATRIB('Barreiras'));
@@ -39,18 +28,12 @@ Insert into APPP_TB_ATRIBUTO_OBJ (CD_TIPO,DS_ATRIBUTO_OBJ,FL_ATRIB_RELAC,NM_ATRI
       values (1,'Consequencias do problema','S','Consequencias',APPP_FN_NM_COL_ATRIB('Consequencias') );
 
 Insert into APPP_TB_ATRIBUTO_OBJ (CD_TIPO,DS_ATRIBUTO_OBJ,FL_ATRIB_RELAC,NM_ATRIBUTO_OBJ,NM_COLUNA) 
-<<<<<<< .mine
       values (1,'Recomendações para o problema','S','Recomendações',APPP_FN_NM_COL_ATRIB( 'Recomendações'));
-=======
-      values (5,'Recomendações para o problema','S','Recomendações',null);
->>>>>>> .r267
+
 
 Insert into APPP_TB_ATRIBUTO_OBJ (CD_TIPO,DS_ATRIBUTO_OBJ,FL_ATRIB_RELAC,NM_ATRIBUTO_OBJ,NM_COLUNA) 
-<<<<<<< .mine
       values (1,'Descrição da Persona','S','Descrição',APPP_FN_NM_COL_ATRIB('Descrição'));
-=======
-      values (5,'Descrição da Persona','S','Descrição',null);
->>>>>>> .r267
+
 
 insert into APPP_TB_ATRIBUTO_OBJ(NM_ATRIBUTO_OBJ,DS_ATRIBUTO_OBJ                      ,CD_TIPO,FL_ATRIB_RELAC,NM_COLUNA) 
                           VALUES('Nome Usuário' ,'Nome completo do usuário do Pattern',1      ,'S',APPP_FN_NM_COL_ATRIB('Nome Usuário') ); 
@@ -63,22 +46,15 @@ insert into APPP_TB_ATRIBUTO_OBJ(NM_ATRIBUTO_OBJ,DS_ATRIBUTO_OBJ                
                           VALUES('Data de Nascimento' ,'Data de Nascimento do usuário do Pattern' ,4      ,'S',APPP_FN_NM_COL_ATRIB('Data de Nascimento')); 
                           
 
-<<<<<<< .mine
 insert into APPP_TB_ATRIBUTO_OBJ(NM_ATRIBUTO_OBJ,DS_ATRIBUTO_OBJ ,CD_TIPO,FL_ATRIB_RELAC,NM_COLUNA) 
                           VALUES('CPF' ,'CPF do Usuário'         ,6      ,'S',APPP_FN_NM_COL_ATRIB('CPF'));  
 
-=======
->>>>>>> .r267
 insert into APPP_TB_ATRIBUTO_OBJ(NM_ATRIBUTO_OBJ        ,DS_ATRIBUTO_OBJ                  ,CD_TIPO,FL_ATRIB_RELAC,NM_COLUNA) 
                           VALUES('Número da Residência' ,'Número da Residência do Usuário',2      ,'S',APPP_FN_NM_COL_ATRIB( 'Número da Residência'));                           
                           
-<<<<<<< .mine
 insert into APPP_TB_ATRIBUTO_OBJ(NM_ATRIBUTO_OBJ,DS_ATRIBUTO_OBJ ,CD_TIPO,FL_ATRIB_RELAC,Nm_Coluna) 
                           VALUES('E-mail' ,'E-mail do Usuário'         ,5      ,'S',APPP_FN_NM_COL_ATRIB( 'E-mail')); 
-=======
-insert into APPP_TB_ATRIBUTO_OBJ(NM_ATRIBUTO_OBJ,DS_ATRIBUTO_OBJ ,CD_TIPO,FL_ATRIB_RELAC) 
-                          VALUES('E-mail' ,'E-mail do Usuário'         ,1      ,'S');
->>>>>>> .r267
+
                            
 insert into APPP_TB_ATRIBUTO_OBJ(NM_ATRIBUTO_OBJ,DS_ATRIBUTO_OBJ ,CD_TIPO,FL_ATRIB_RELAC,Nm_Coluna) 
                           VALUES('RG' ,'RG do Usuário'           ,9      ,'S',APPP_FN_NM_COL_ATRIB('RG'));                           
