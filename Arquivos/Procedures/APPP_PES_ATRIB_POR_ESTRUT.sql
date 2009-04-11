0a61021edd3974fa71f7cbb3c4adc1d76e2c0f4e
guilhermewachs <guilhermewachs@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-04-11 00:08:09 +0000

    A ordem em que os atributos apareciam na estrutura estava invertida. Um order by foi adicionado para manter a ordem correta.

--- Arquivos/Procedures/APPP_PES_ATRIB_POR_ESTRUT.sql
+++ Arquivos/Procedures/APPP_PES_ATRIB_POR_ESTRUT.sql
@@ -3,6 +3,7 @@
 * APPP_PES_ATRIB_POR_ESTRUT : Procedure para PESQUISAR dados de ATRIBUTOS POR CÓDIGO DE ESTRUTURA
 * Author                    : WeeDo 
 * History                   : 09/03/2009 - Matheus Gonçalves
+*                           : 10/04/2009 - Guilherme Wachs Lopes - 'Order by ROWNUM desc'
 ***********************************************************************************************************************/
 create or replace procedure APPP_PES_ATRIB_POR_ESTRUT(pCD_ESTRUTURA    IN NUMBER, 
                                                       p_cursor    OUT SYS_REFCURSOR   ) is
@@ -27,7 +28,8 @@ begin
        where EO.cd_estrutura    = pCD_ESTRUTURA
        AND   AE.CD_ESTRUTURA    = EO.CD_ESTRUTURA
        AND   AO.CD_ATRIBUTO_OBJ = AE.CD_ATRIBUTO_OBJ
-       AND   T.CD_TIPO          = AO.CD_TIPO;
-       
+       AND   T.CD_TIPO          = AO.CD_TIPO
+       ORDER BY ROWNUM DESC; --Inverte os resultados para aparecer na ordem correta.
+        
 end APPP_PES_ATRIB_POR_ESTRUT;
 /
