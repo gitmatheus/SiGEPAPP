24913da1f4508cfd9aff08fba88e57c93e3b0299
toadgeek <toadgeek@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-05-07 04:52:51 +0000

    Correcao dos Scripts de criacao do banco. Favor baixar toda a pasta Arquivos para C:\SiGEPAPP e executar o script 00-RUN_CREATE_BD.sql

--- Arquivos/Insercoes/04-INS_APPP_TB_LOGIN.sql
+++ Arquivos/Insercoes/04-INS_APPP_TB_LOGIN.sql
@@ -1,5 +1,4 @@
 REM INSERTING into APPP_TB_LOGIN
-Insert into APPP_TB_LOGIN (CD_USER,NM_LOGIN,PW_SENHA) values (35140189879,'admin','admin');
-Insert into APPP_TB_LOGIN (CD_USER,NM_LOGIN,PW_SENHA) values (30150215835,'toadpunk','toadpunk');
+Insert into APPP_TB_LOGIN (CD_USER,NM_LOGIN,PW_SENHA) values (65796803859,'admin','WkdbV1U=');
 
 
