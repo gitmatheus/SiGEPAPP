9348d471bdf86db6550e87ddc0cea1a23db40fc1
toadgeek <toadgeek@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-05-13 20:48:57 +0000

    Alteracoes e consideracoes para finalizacao do banco.

--- Arquivos/00-RUN_CREATE_BD.sql
+++ Arquivos/00-RUN_CREATE_BD.sql
@@ -7,6 +7,17 @@ spool /SiGEPAPP/Arquivos/LogCREATEBD.log
 
 @ /SiGEPAPP/Arquivos/ScriptsDrop/00-DROP_SCRIPT.SQL
 
+@ /SiGEPAPP/Arquivos/ScriptsDrop/01-DROP_SCRIPT.SQL
+@ /SiGEPAPP/Arquivos/ScriptsDrop/02-DROP_SCRIPT.SQL
+@ /SiGEPAPP/Arquivos/ScriptsDrop/02a-DROP_SCRIPT.SQL
+@ /SiGEPAPP/Arquivos/ScriptsDrop/03-DROP_SCRIPT.SQL
+@ /SiGEPAPP/Arquivos/ScriptsDrop/04-DROP_SCRIPT.SQL
+@ /SiGEPAPP/Arquivos/ScriptsDrop/05-DROP_SCRIPT.SQL
+@ /SiGEPAPP/Arquivos/ScriptsDrop/06-DROP_SCRIPT.SQL
+@ /SiGEPAPP/Arquivos/ScriptsDrop/07-DROP_SCRIPT.SQL
+@ /SiGEPAPP/Arquivos/ScriptsDrop/08-DROP_SCRIPT.SQL
+@ /SiGEPAPP/Arquivos/ScriptsDrop/09-DROP_SCRIPT.SQL
+
 @ /SiGEPAPP/Arquivos/ScriptsBanco/00-RUN_SCRIPTS.sql
 
 @ /SiGEPAPP/Arquivos/Functions/00-RUN_FUNCTIONS.sql
