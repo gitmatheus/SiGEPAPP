c986bff62af97eac720838ba2b7f60366c98cd48
toadgeek <toadgeek@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-05-13 16:40:04 +0000

    Alteracao em procedure de insercao na tabela APPP_TB_RELAC_PERG_RESP, dando a opcao de update em caso de erro ao inserir o dado já existente.

--- Arquivos/Procedures/APPP_INS_RELAC_PERG_RESP.sql
+++ Arquivos/Procedures/APPP_INS_RELAC_PERG_RESP.sql
@@ -1,13 +1,15 @@
 /**************************************************************************************
 * Project Name             : SiGEPAPP
-* APPP_INS_RELAC_PERG_RESP : Procedure para criação de dados de RESPOSTA x PERGUNTAS
+* APPP_INS_RELAC_PERG_RESP : Procedure para criacao de dados de RESPOSTA x PERGUNTAS
 *                            vResult( 1=OK; -99=ErroGeral)                     
 * Author                   : WeeDo 
-* History                  : 06/03/2009 - Matheus Gonçalves
+* History                  : 06/03/2009 - Matheus Goncalves - Versao Inicial
+*                          : 13/05/2009 - Matheus Goncalves - Opcao de atualizacao
 ***************************************************************************************/
 create or replace procedure APPP_INS_RELAC_PERG_RESP(pCD_PERGUNTA   IN NUMBER,
                                                      pCD_RESPOSTA   IN NUMBER,
                                                      pNR_VALOR_RESP IN NUMBER,
+																										 OP_UPDATE      IN VARCHAR2 DEFAULT 'N',
                                                      vResult     out number) is
 vERRO        VARCHAR2(600);
 begin
@@ -24,11 +26,14 @@ begin
    vResult := 1; -- OK
    commit;
    
-   EXCEPTION
-     WHEN OTHERS THEN
+   EXCEPTION 
+	   WHEN OTHERS THEN
         rollback;
-        vResult := SQLCODE; -- Erro generico.
-        vERRO   := SUBSTR(SQLERRM,600);
-               
+				IF OP_UPDATE = 'S' AND SQLCODE = -1 THEN
+				   APPP_UPD_RELAC_PERG_RESP(pCD_PERGUNTA,pCD_RESPOSTA, pNR_VALOR_RESP,vResult);
+				ELSE   
+           vResult := SQLCODE; -- Erro generico.
+           vERRO   := SUBSTR(SQLERRM,600);
+        END IF;       
 end APPP_INS_RELAC_PERG_RESP;
 /
