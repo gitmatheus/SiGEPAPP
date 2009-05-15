84268292eef059aeae16ae01671c5402fa9edd0e
andreyam <andreyam@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-05-15 00:20:32 +0000

    Servlet Tom Mix!!!

--- src/br/edu/fei/sigepapp/servlet/GetRelacPergRespServlet.java
+++ src/br/edu/fei/sigepapp/servlet/GetRelacPergRespServlet.java
@@ -1,7 +1,11 @@
 package br.edu.fei.sigepapp.servlet;
 
+import br.edu.fei.sigepapp.bancodedados.dao.PerguntaDAO;
 import br.edu.fei.sigepapp.bancodedados.dao.RelacPergRespDAO;
+import br.edu.fei.sigepapp.bancodedados.dao.RespostaDAO;
+import br.edu.fei.sigepapp.bancodedados.model.Pergunta;
 import br.edu.fei.sigepapp.bancodedados.model.Relac_Perg_Resp;
+import br.edu.fei.sigepapp.bancodedados.model.Resposta;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.SQLException;
@@ -23,31 +27,62 @@ public class GetRelacPergRespServlet extends HttpServlet {
             throws ServletException, IOException {
         response.setContentType("text/xml;charset=UTF-8");
         PrintWriter out = response.getWriter();
+        Boolean erro = false;
+        String xml = new String();
 
         try {
             RelacPergRespDAO relacPergRespDAO = new RelacPergRespDAO();
             List<Relac_Perg_Resp> listRelacPergResp = relacPergRespDAO.APPP_SEL_RELAC_PERG_RESP(new Relac_Perg_Resp());
+            relacPergRespDAO.fechaConexao();
+
+            Pergunta p = new Pergunta();
+            Resposta r = new Resposta();
 
             out.println("<xml>");
+            xml += "<RelacPergResp>";
 
             for (Relac_Perg_Resp relacPergResp : listRelacPergResp) {
-                out.println("<RelacPergResp>");
-                out.println("<CDPerg>");
-                out.println(relacPergResp.getCd_pergunta());
-                out.println("</CDPerg>");
-                out.println("<CDResp>");
-                out.println(relacPergResp.getCd_resposta());
-                out.println("</CDResp>");
-                out.println("<PesoResp>");
-                out.println(relacPergResp.getNro_valor_resp());
-                out.println("</PesoResp>");
-                out.println("</RelacPergResp>");
+                xml += "<CDPerg>";
+                xml += relacPergResp.getCd_pergunta();
+                xml += "</CDPerg>";
+                p.setCd_pergunta(relacPergResp.getCd_pergunta());
+                PerguntaDAO pDAO = new PerguntaDAO();
+                List<Pergunta> listP = pDAO.APPP_SEL_PERGUNTA(p);
+                pDAO.fechaConexao();
+                if (listP.size() > 1) {
+                    out.println("<erro>1</erro>");
+                    erro = true;
+                } else {
+                    for (Pergunta x : listP) {
+                        xml += "<DSPerg>";
+                        xml += x.getDs_pergunta();
+                        xml += "</DSPerg>";
+                    }
+                }
+                xml += "<CDResp>";
+                xml += relacPergResp.getCd_resposta();
+                xml += "</CDResp>";
+                RespostaDAO rDAO = new RespostaDAO();
+                r.setCd_resposta(relacPergResp.getCd_resposta());
+                List<Resposta> listR = rDAO.APPP_SEL_RESPOSTA(r);
+                rDAO.fechaConexao();
+                if (listR.size() > 1) {
+                    out.println("<erro>1</erro>");
+                    erro = true;
+                } else {
+                    for (Resposta x : listR) {
+                        xml += "<DSResp>";
+                        xml += x.getDs_resposta();
+                        xml += "</DSResp>";
+                    }
+                }
+
             }
 
+            xml += "</RelacPergResp>";
+            out.println(xml);
             out.println("</xml>");
-
             out.flush();
-            relacPergRespDAO.fechaConexao();
 
         } catch (SQLException ex) {
             Logger.getLogger(GetPerguntaServlet.class.getName()).log(Level.SEVERE, null, ex);
