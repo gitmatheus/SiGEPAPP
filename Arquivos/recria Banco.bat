e49737d129e5ee1e2ea3a7b9e99e57dfbe5bcf9d
andreyam <andreyam@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-05-21 12:00:58 +0000

    Bat de criação do Banco de Dados

--- Arquivos/recria Banco.bat
+++ Arquivos/recria Banco.bat
@@ -1,8 +1,17 @@
-if exist c:\sigepapp\Arquivos del c:\sigepapp\Arquivos\*.* /s
+@echo off
 
-if not exist c:\sigepapp\Arquivos md c:\sigepapp\Arquivos
+if exist c:\sigepapp\Arquivos (
+@echo Apagando os arquivos existentes
+rd /s /y "c:\sigepapp\Arquivos\*.* " 
+del /s "c:\sigepapp\Arquivos\*.* "
+)
 
+if not exist c:\sigepapp\Arquivos md "c:\sigepapp\Arquivos"
+
+@echo Efetuando copia dos arquivos
 cd ..
 xcopy "Arquivos" "c:\sigepapp\Arquivos\" /s /v
 
+@echo Executando SQL
 sqlplus.exe System/System@xe @c:/sigepapp/arquivos/00-run_create_bd.sql
+exit
