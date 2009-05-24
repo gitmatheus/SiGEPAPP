6f007b7998e1a27f786700ef1f195f4b5485ad99
guilhermewachs <guilhermewachs@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-05-24 02:25:54 +0000

    Tamanho das fontes gravadas no Cookies para lembrar as escolhas dos usuários

--- web/js/appp_funcoes.js
+++ web/js/appp_funcoes.js
@@ -44,22 +44,57 @@
  * |------------------------------------------------------------------|
  */
 
+/*
+     *Funções para Gravar e Ler Cookies*/
+
+function createCookie(name,value,days) {
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime()+(days*24*60*60*1000));
+        var expires = "; expires="+date.toGMTString();
+    }
+    else expires = "";
+    document.cookie = name+"="+value+expires+"; path=/";
+}
+
+function readCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+
+
+
 /** As funcoes contidas dentro do metodo ready, sao poderam ser executadas apos o carregamento da pagina */
 $(document).ready(function(){
 
     /*Função de acessibilidade*/
-    var Tamanhos_Texto = new Array( '12px','14px','15px','16px' );
     var Tamanho_Texto_Atual = 0;
+    var Tamanho_Historico=readCookie("Tamanho_Fonte");
+    var Tamanhos_Texto = new Array( '12px','14px','15px','16px' );
+    if(Tamanho_Historico!=null){
+        Tamanho_Texto_Atual=Tamanho_Historico;
+        $("*").css("font-size", Tamanhos_Texto[Tamanho_Texto_Atual]);
+    }
+
     $("#link_aumenta_fonte").click(function(){
         if(Tamanho_Texto_Atual<3){
             Tamanho_Texto_Atual++;
+            createCookie("Tamanho_Fonte", Tamanho_Texto_Atual);
             $("*").css("font-size", Tamanhos_Texto[Tamanho_Texto_Atual]);
         }
     });
     $("#link_diminui_fonte").click(function(){
         if(Tamanho_Texto_Atual>0){
-        Tamanho_Texto_Atual--;
-        $("*").css("font-size", Tamanhos_Texto[Tamanho_Texto_Atual]);
+            Tamanho_Texto_Atual--;
+            createCookie("Tamanho_Fonte", Tamanho_Texto_Atual);
+            $("*").css("font-size", Tamanhos_Texto[Tamanho_Texto_Atual]);
+         
         }
     });
 
